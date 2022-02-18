88ba0a13e63aa2b2bb942a4b810be705fd765f32
Will English <80693248+meranbioph@users.noreply.github.com>
2022-02-18 22:02:44 +0100

    Initial upload

---
+++ 990_Cd_Cv_hr_server_Shiny.R
@@ -0,0 +1,58 @@
+############################################
+############################################
+##
+## NBR Weather data - Shiny App
+##
+## This project uses R 4.1.2 
+## with snapshot date 2021-11-01
+##
+############################################
+############################################
+
+# Setup
+
+{
+  # -------------------------------------------
+  snapshot_date = "2021-11-01"
+  options("repos" = paste0("https://mran.revolutionanalytics.com/snapshot/", snapshot_date))
+  # -------------------------------------------
+  
+  # -------------------------------------------
+  # sink options
+  options(width = 150)
+  # rJava memory option
+  options(java.parameters = "-Xmx8000m")
+  # -------------------------------------------
+  
+  # R packages
+  # -------------------------------------------
+  Rpackages_version = c("data.table_1.14.2",
+                        "dplyr_1.0.7",
+                        "ggplot2_3.3.5",
+                        "tidyr_1.1.4",
+                        "writexl_1.4.0",
+                        "rdrop2_0.8.2.1",
+                        "shiny_1.7.1",
+                        "plotly_4.10.0"
+  )
+  path_Rpackages = "C:/R packages_412"
+  # -------------------------------------------
+  
+  # version check and load packages
+  # -------------------------------------------
+  # R version check
+  if(sessionInfo()$R.version$version.string != "R version 4.1.2 (2021-11-01)") stop("R.version must be 4.1.2 (2021-11-01)")
+  
+  # install packages
+  Rpack = sapply(strsplit(Rpackages_version, "_", fixed = T), FUN = function(x) x[1])
+  Rpack_version = sapply(strsplit(Rpackages_version, "_", fixed = T), FUN = function(x) x[2])
+  if(!all(Rpack %in% list.files(path_Rpackages))){
+    loadRpackages <- Rpack[!Rpack %in% list.files(path_Rpackages)]
+    for(i in loadRpackages) install.packages(i, lib = path_Rpackages, repos = options("repos"), dependencies = T)
+  }
+  
+  # load packages
+  for(i in Rpack) eval(parse(text = paste0("library(", i, ", lib.loc = '", path_Rpackages, "')")))
+}
+
+############################################
